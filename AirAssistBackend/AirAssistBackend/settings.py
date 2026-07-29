11b3e9df4b1a8f38287084924eabef9767c4bc58
Calin Brezeanu <teodorbrezeanu2004@gmail.com>
2026-07-29 14:58:37 +0200

    Hid the Database credentials

--- AirAssistBackend/AirAssistBackend/settings.py
+++ AirAssistBackend/AirAssistBackend/settings.py
@@ -9,8 +9,11 @@ https://docs.djangoproject.com/en/6.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
-
+import os
 from pathlib import Path
+from dotenv import load_dotenv
+
+load_dotenv()  # Load environment variables from .env file
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -76,11 +79,11 @@ WSGI_APPLICATION = 'AirAssistBackend.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'AirAssist',
-        'USER': 'postgres',
-        'PASSWORD': 'CalinT23',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
     }
 }
 
