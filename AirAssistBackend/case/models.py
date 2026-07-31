25ca9be783b444defddf49fe5d810a6a154d52c4
Gabriel Man <mangabriel03@gmail.com>
2026-07-31 16:27:54 +0300

    model case

--- AirAssistBackend/case/models.py
+++ AirAssistBackend/case/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+
+class Case(models.Model):
+
+    class Status(models.TextChoices):
+        NEW = "NEW", "New"
+        INCOMPLETE = "INCOMPLETE", "Incomplete"
+        VALID = "VALID", "Valid"
+        REJECTED = "REJECTED", "Rejected" 
+
+    status = models.CharField(
+        max_length=50,
+        choices=Status.choices,
+        default=Status.NEW,
+    )
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ["-created_at"]
+
+    def __str__(self):
+        return f"Case{self.id}"
