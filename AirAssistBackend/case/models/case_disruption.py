e92adbaf57784ed8f41d1532e94baf680f835291
Catalina Vasiu <vasiu.catalina10@gmail.com>
2026-08-03 18:09:52 +0200

    add fields cancellation, delay, denied boarding, denied boarding reason in case disruption model

--- AirAssistBackend/case/models/case_disruption.py
+++ AirAssistBackend/case/models/case_disruption.py
@@ -1,15 +1,48 @@
 
 
 from django.db import models
-from disruption_type import DisruptionMotive
+from .disruption_type import DisruptionMotive
+from .cancellation_type import CancellationType
+from .delay_type import DelayType
+from .denied_boarding_type import DeniedBoardingType
+from .denied_boarding_reason_type import DeniedBoardingReasonType
+
 
 class Disruption(models.Model):
-    case = models.ForeignKey("Case", on_delete=models.CASCADE, related_name="distruptions",)
+    case = models.ForeignKey("Case", on_delete=models.CASCADE, related_name="disruptions",)
     motive = models.CharField(
         max_length=20,
         choices=DisruptionMotive.choices(),
     )
 
+    cancellation_type = models.CharField(
+        max_length = 30,
+        choices = CancellationType.choices(),
+        null = True,
+        blank = True,
+    )
+
+    delay_type = models.CharField(
+        max_length = 30,
+        choices = DelayType.choices(),
+        null = True,
+        blank = True,
+    )
+
+    denied_boarding_type = models.CharField(
+        max_length = 10,
+        choices = DeniedBoardingType.choices(),
+        null = True,
+        blank = True,
+    )
+
+    denied_boarding_reason = models.CharField(
+        max_length = 30,
+        choices = DeniedBoardingReasonType.choices(),
+        null = True,
+        blank = True,
+    )
+    
     incident_description = models.TextField(null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
