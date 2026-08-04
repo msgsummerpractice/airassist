176884c778e47cfd6b3ab0d376ed422b3f25ee4d
Catalina Vasiu <vasiu.catalina10@gmail.com>
2026-08-04 18:00:40 +0200

    enforce must_change_password model field in LoginView. implement ChangePasswordView to change the inital generated password with a new password. add in urls the path for change password.

--- AirAssistBackend/user/urls.py
+++ AirAssistBackend/user/urls.py
@@ -13,4 +13,5 @@ urlpatterns = [
     path("login/", views.LoginView.as_view(), name="login"),
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('change-password/', views.ChangePasswordView.as_view(), name='change_password'),
 ]
