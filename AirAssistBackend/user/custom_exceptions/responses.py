cea4a9f1f35c4cfdf0edfb5a46026e51b54a5ed2
Calin Brezeanu <teodorbrezeanu2004@gmail.com>
2026-08-11 14:42:40 +0200

    deleted useless responses

--- AirAssistBackend/user/custom_exceptions/responses.py
+++ AirAssistBackend/user/custom_exceptions/responses.py
@@ -11,20 +11,6 @@ class AirAssistResponse:
     def status_ok(self,data):
         return Response(data,status=status.HTTP_200_OK)
 
-    def status_login_success(self, refresh):
-        return {
-            "message": "Successfully logged in",
-            "access": str(refresh.access_token),
-            "refresh": str(refresh),
-        }
-
-    def status_forbidden_with_message(self, message):
-        return Response({"error": message}, status=status.HTTP_403_FORBIDDEN)
-
-    def status_bad_request(self, data):
-        return Response({"error": "Bad request", "data": data}, status=status.HTTP_400_BAD_REQUEST)
-
-    def status_bad_request_with_message(self, message):
-        return Response({"error": message}, status=status.HTTP_400_BAD_REQUEST)
+    
 
 
