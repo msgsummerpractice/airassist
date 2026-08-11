3f6e9a03f964863454e6faeb671b00756f1bc85b
Calin Brezeanu <teodorbrezeanu2004@gmail.com>
2026-08-11 14:29:51 +0200

    Implemented Create User Accounts from Sprint 1. Added build new user form,show succesufl creation message and assured that user is stored in databse

--- AirAssistBackend/user/custom_exceptions/responses.py
+++ AirAssistBackend/user/custom_exceptions/responses.py
@@ -3,7 +3,7 @@ from rest_framework.response import Response
 
 class AirAssistResponse:
     def status_create(self, data):
-        return Response(data,status=status.HTTP_201_CREATED)
+        return Response({"message": "User account created successfully.","data": data},status=status.HTTP_201_CREATED)
 
     def status_no_content(self):
         return Response({'message':f"Request completed successfully"},status=status.HTTP_204_NO_CONTENT)
@@ -11,8 +11,20 @@ class AirAssistResponse:
     def status_ok(self,data):
         return Response(data,status=status.HTTP_200_OK)
 
-    def status_login_success(self):
-        return Response({"Successfully logged in"},
-        status=status.HTTP_200_OK)  
+    def status_login_success(self, refresh):
+        return {
+            "message": "Successfully logged in",
+            "access": str(refresh.access_token),
+            "refresh": str(refresh),
+        }
 
-        
+    def status_forbidden_with_message(self, message):
+        return Response({"error": message}, status=status.HTTP_403_FORBIDDEN)
+
+    def status_bad_request(self, data):
+        return Response({"error": "Bad request", "data": data}, status=status.HTTP_400_BAD_REQUEST)
+
+    def status_bad_request_with_message(self, message):
+        return Response({"error": message}, status=status.HTTP_400_BAD_REQUEST)
+
+
