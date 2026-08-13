59add2c088df6afc75b5e301eca947bf0af2114f
Catalina Vasiu <vasiu.catalina10@gmail.com>
2026-08-13 13:25:22 +0300

    implement Passenger Case Details View. add routing in url.

--- AirAssistBackend/case/urls.py
+++ AirAssistBackend/case/urls.py
@@ -22,6 +22,7 @@ from .views.case_creation_view import CaseCreationView
 from .views.case_eligibility_view import CaseEligibilityView
 from .views.case_assigment_view import CaseAssignmentView
 from .views.passenger_case_list_view import PassengerCaseListView
+from .views.passenger_case_details_view import PassengerCaseDetailsView
 
 urlpatterns = [
     path('cases/', CaseCreationView.as_view(), name='case-create'),
@@ -30,4 +31,5 @@ urlpatterns = [
     path("cases/me/", PassengerCaseListView.as_view(), name="passenger-case-list"),
     path("cases/eligibility-check/", CaseEligibilityView.as_view(), name="case-eligibility-check"),
     path("cases/<int:case_id>/assign/", CaseAssignmentView.as_view(), name="case-assign"),
-]
+    path("cases/me/<int:pk>/", PassengerCaseDetailsView.as_view(), name="passenger-case-details"),
+] 
