f4034aa71379cf6e6cf21ee5a0c50c3a672ad402
darius soponar <soponar.darius@gmail.com>
2026-08-17 12:36:00 +0300

    add downloading logic for files in the case passanger details view

--- AirAssistBackend/case/urls.py
+++ AirAssistBackend/case/urls.py
@@ -22,6 +22,7 @@ from .views.case_creation_view import CaseCreationView
 from .views.case_eligibility_view import CaseEligibilityView
 from .views.case_assigment_view import CaseAssignmentView
 from .views.passenger_case_comment_view import PassengerCaseCommentCreateView
+from .views.passenger_case_document_download_view import PassengerCaseDocumentDownloadView
 from .views.passenger_case_list_view import PassengerCaseListView
 from .views.passenger_case_details_view import PassengerCaseDetailsView
 
@@ -33,5 +34,10 @@ urlpatterns = [
     path("cases/eligibility-check/", CaseEligibilityView.as_view(), name="case-eligibility-check"),
     path("cases/<int:case_id>/assign/", CaseAssignmentView.as_view(), name="case-assign"),
     path("cases/me/<int:pk>/", PassengerCaseDetailsView.as_view(), name="passenger-case-details"),
+    path(
+        "cases/me/<int:pk>/documents/<int:document_id>/download/",
+        PassengerCaseDocumentDownloadView.as_view(),
+        name="passenger-case-document-download",
+    ),
     path("cases/me/<int:pk>/comments/", PassengerCaseCommentCreateView.as_view(), name="passenger-case-comment-create"),
 ] 
